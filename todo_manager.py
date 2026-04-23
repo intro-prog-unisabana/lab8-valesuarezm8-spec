defd01026e818855e962faa4443c303f8c812d4e
Valeria Suarez Moreno <valesmuniversidad@gmail.com>
2026-04-23 11:52:01 -0500

    5

--- todo_manager.py
+++ todo_manager.py
@@ -5,12 +5,10 @@ def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
     # TODO: Implementar manejo de FileNotFoundError según README.md
     try:
-        # Intentar abrir el archivo
         with open(file_path, 'r') as file:
             return file.read().splitlines()
     
     except FileNotFoundError:
-        # Si el archivo no existe
         print(f"File {file_path} not found! Returning an empty to-do list.")
         return []
     raise NotImplementedError
@@ -20,8 +18,6 @@ def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
     # TODO: Implementar escritura de tareas según README.md
     with open(file_path, 'w') as file:
-        if tasks:
-            file.write('\n'.join(tasks) + '\n')
-        else:
-            file.write('')
-    raise NotImplementedError
+        for task in tasks:
+            file.write(task + '\n')
+   
